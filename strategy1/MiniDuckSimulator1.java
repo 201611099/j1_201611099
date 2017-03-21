3d2643ce8712f461abc3e400771d4d53f9bddb31
201611099 이효정 <yan04121@naver.com>
2017-03-21 19:05:06 +0900

    Delete MiniDuckSimulator1.java

--- strategy1/MiniDuckSimulator1.java
+++
@@ -1,9 +0,0 @@
-package headfirst.strategy1;
-
-public class MiniDuckSimulator1 {
-  public static void main(String[] args) {
-    Duck mallard= new MallardDuck();
-    mallard.performFly();
-    mallard.performQuack();
-  }
-}
