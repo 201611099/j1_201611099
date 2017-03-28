6e33b82aaaf6998f26cec28156950b6971fae5cb
201611099 이효정 <yan04121@naver.com>
2017-03-28 20:36:57 +0900

    Update ReverseComparator.java

--- src/com/sd/sort/v2/ReverseComparator.java
+++ src/com/sd/sort/v2/ReverseComparator.java
@@ -2,7 +2,7 @@ package com.sd.sort.v2;
 import java.util.Date;
 import com.sd.turtle.WeightTurtle;
 
-class ReverseComparator implements Comparator {
+public class ReverseComparator implements Comparator {
     private final Comparator c;
     public ReverseComparator(Comparator c) {this.c = c; }
     public int compare(Object o1, Object o2) {
