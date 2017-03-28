eda33de8d5613bd7e8583c10413b9795c4a8b1d8
201611099 이효정 <yan04121@naver.com>
2017-03-28 20:36:30 +0900

    Update IntegerComparator.java

--- src/com/sd/sort/v2/IntegerComparator.java
+++ src/com/sd/sort/v2/IntegerComparator.java
@@ -2,7 +2,7 @@ package com.sd.sort.v2;
 import java.util.Date;
 import com.sd.turtle.WeightTurtle;
 
-class IntegerComparator implements Comparator {
+public class IntegerComparator implements Comparator {
     public IntegerComparator() {}
     public int compare(Object o1, Object o2) {
         return (Integer)o1 - (Integer)o2;
