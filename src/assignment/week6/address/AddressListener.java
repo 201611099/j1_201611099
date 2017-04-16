55667174613f530119a2a64eea1cacadb1c725dc
201611099 이효정 <yan04121@naver.com>
2017-04-16 21:32:11 +0900

    Update AddressListener.java

--- src/assignment/week6/address/AddressListener.java
+++ src/assignment/week6/address/AddressListener.java
@@ -6,31 +6,7 @@ import java.awt.*;
 import java.util.*;
 
 public class AddressListener implements ActionListener { 
-  /*private String name;
-  private String phone;
-  private String email;
-  
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
-  public String getPhone() {
-    return phone;
-  }
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
-  public String getEmail() {
-    return email;
-  }
-  public void setEmail(String email) {
-    this.email = email;
-  }*/
-  //private JTextField nameTextField, phoneTextField, emailTextField;
   public void actionPerformed(ActionEvent e) {
-    //private JTextField nameTextField, phoneTextField, emailTextField;
     JButton b = (JButton)e.getSource();
     String cmd = e.getActionCommand();
     JTextField nameTextField = new JTextField(25);
