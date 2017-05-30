407c077209ca3a7a9754e0e9f8b1658cb982eba0
201611099 이효정 <yan04121@naver.com>
2017-05-30 14:50:04 +0900

    Update DaoMainV0.java

--- src/com/sd/dao/v0/DaoMainV0.java
+++ src/com/sd/dao/v0/DaoMainV0.java
@@ -10,6 +10,7 @@ public class DaoMainV0{
   public static void main(String[] args) {
     Connection conn = null;
     Statement stmt = null;
+    ResultSet rs = null;
     try {
       String path = System.getProperty("user.dir");
       path+="/src/mysql.properties";
@@ -19,21 +20,26 @@ public class DaoMainV0{
       //static Connection getConnection(String url, String user, String password)
       //conn=DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "password");
       conn = DriverManager.getConnection(
-        prop.getProperty("URL"),
+        prop.getProperty("URL")/*+"?useSSL=true"*/,
         prop.getProperty("USER"),
         prop.getProperty("PASSWORD")
       );  
+      //String mySql = "CREATE TABLE persons(id int(64) NOT NULL AUTO_INCREMENT,";
+      //mySql+="name varchar(255) NOT NULL, address varchar(255) NOT NULL, UNIQUE (id))";
+      //String mySql=("INSERT INTO persons(name,address) VALUE('v0-1', '1 Hongji Dong')");
+      String mySql = "select name from persons where id<4 order by id";
       stmt=conn.createStatement();
-      String mySql = "CREATE TABLE persons(id int(64) NOT NULL AUTO_INCREMENT,";
-      mySql+="name varchar(255) NOT NULL, address varchar(255) NOT NULL, UNIQUE (id))";
-      stmt.execute(mySql);
+      //stmt.execute(mySql);
+      rs = stmt.executeQuery(mySql);
+      while(rs.next()) {
+        System.out.println(rs.getString("name"));
+      }
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
       try{
+        if(rs != null) rs.close();
         if(stmt != null) stmt.close();
-      } catch(Exception e) {};
-      try{
         if(conn != null) conn.close();
       } catch(Exception e) {};
     }
